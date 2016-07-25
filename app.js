8d291d4c7551efd404ab68eb31e1bad871140f5f
lizlove <elizabethlovero@gmail.com>
2016-07-25 14:41:10 -0400

    Add express static server, gzip compression, and cache control.

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
-var h5bp = require('h5bp');
-
-var app = h5bp.createServer({ root: __dirname + '/public' });
-app.listen(3000);
-
 var express = require('express'),
-    h5bp = require('h5bp');
+    compression = require('compression')
+;
 
 var app = express();
-app.use(h5bp({ root: __dirname + '/public' }));
 
-// in order to serve files, you should add the two following middlewares
-app.use(express.compress());
-app.use(express.static(__dirname + '/public'));
-app.listen(3000);
+var oneDay = 86400000;
+
+app.use(compression());
+app.use(express.static(__dirname + '/public', { maxAge: oneDay }));
+
+app.listen(process.env.PORT || 3000);
